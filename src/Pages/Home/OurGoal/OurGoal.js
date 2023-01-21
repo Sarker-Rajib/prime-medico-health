d2792cc13ca4fc86de11d6ccc816b1b773d7f989
Rajib Sarker <redhat0593@gmail.com>
2023-01-22 00:06:31 +0600

    ourGoal done

--- src/Pages/Home/OurGoal/OurGoal.js
+++ src/Pages/Home/OurGoal/OurGoal.js
@@ -14,6 +14,13 @@ const OurGoal = () => {
                     <Col lg={6}>
                         <div className="text-end">
                             <h2 className='section-header-rtl'>Our Goal</h2>
+                            <div className="text-start">
+                                <p>It's very clear that we are always beside you to save you from unwanted things we face like diseases and accidents.
+                                    We belive God always beside us and we can help each others.
+                                </p>
+                                <p>With minimum cost, our aim is to give you the mest medical services.</p>
+                                <p>We are commited to reach at your doorstep, You just need to call us for once.</p>
+                            </div>
                         </div>
                     </Col>
                 </Row>
