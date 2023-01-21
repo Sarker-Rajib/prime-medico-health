5cc917e9b1496738813ff7c0b6f1e32ccfd35a0a
Rajib Sarker <redhat0593@gmail.com>
2023-01-21 21:57:36 +0600

    our doctor section created

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
 import Banner from './Banner/Banner';
+import OurDoctors from './OurDoctors/OurDoctors';
 
 const Home = () => {
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+
     return (
         <div>
             <Banner></Banner>
+            <OurDoctors></OurDoctors>
         </div>
     );
 };
---
+++ src/Pages/Home/OurDoctors/OurDoctors.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
+import doctor from '../../../Assets/Images/doctor.png';
+import './OurDoctor.css';
+
+const OurDoctors = () => {
+    return (
+        <div className='doctor-banner'>
+            <Container>
+                <Row>
+                    <Col lg={6}>
+                        <div className="text">
+                            <h3 className='section-header'>Our Doctors</h3>
+                            <p>We are commited to give you best Services in this era.</p>
+                            <p>So, here in our healthcare servise you will find Best doctors who are experienced with modern medication over the world.
+                                They are well trained from diffrent foreign medical centers </p>
+                            <p>You will be able to take appoinmnts for the following departmental doctors.</p>
+
+                            <ul className='department-list'>
+                                <li>Oncology</li>
+                                <li>Neurology</li>
+                                <li>Gynaecology</li>
+                                <li>Neurosurgery</li>
+                                <li>Orthopaedics</li>
+                                <li>Plastic Surgery</li>
+                                <li>Gastroenterology</li>
+                                <li>General medicine</li>
+                                <li>ENT(Ear, Nose, Throat)</li>
+                                <li>Cardiology and Cardiac Surgery</li>
+                            </ul>
+                            <p>Explore Our Doctors and choose yours -</p>
+                            <Button>Visit Our Doctors</Button>
+                        </div>
+                    </Col>
+                    <Col lg={6} className="text-center">
+                        <Image src={doctor} className="img-fluid" />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default OurDoctors;
